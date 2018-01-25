ee8606ff7d6c2c1e015385eb32112dfa15a4672f
zhaotengfu <923822745@qq.com>
2018-01-25 15:57:21 +0800

    change

---
+++ src/java/com/sd/annotation/TestA.java
@@ -0,0 +1,12 @@
+package com.sd.annotation;
+
+/**
+ * @author ztf
+ * @version 1.0
+ *          <p>
+ *          2018-1-25
+ *          </P>
+ */
+public @interface TestA {
+
+}
---
+++ src/java/com/sd/service/Userservice.java
@@ -0,0 +1,7 @@
+package com.sd.service;
+
+/**
+ * Created by ztf on 2018-1-16.
+ */
+public interface Userservice {
+}
