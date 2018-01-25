57d03301288ea32f3f7dc1830bf0e5ccbd1cac01
zhaotengfu <923822745@qq.com>
2018-01-25 15:49:43 +0800

    Revert "Create a simple framework"
    This reverts commit dee427111a6c67661cf8cdc2fbe905ab71962be2.

--- src/java/com/sd/service/Userservice.java
+++
@@ -1,7 +0,0 @@
-package com.sd.service;
-
-/**
- * Created by ztf on 2018-1-16.
- */
-public interface Userservice {
-}
--- src/java/com/sd/service/impl/UserserviceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.sd.service.impl;
-
-/**
- * Created by ztf on 2018-1-16.
- */
-public class UserserviceImpl {
-}
